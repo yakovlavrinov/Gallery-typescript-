a5efe00f645aafc12ca4794de63736c89c2268eb
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2024-01-20 19:42:57 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Gallery from "./components";
+import { WolfGallery } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <Gallery
+      <WolfGallery
         photos={[
           {
             id: 1,
@@ -12,34 +12,40 @@ function App() {
             description: "Norway 1",
           },
           {
-            id: 1,
-            src: "/photos/1.jpg",
-            preview: "/photos/preview/1.jpg",
-            description: "Norway 1",
+            id: 2,
+            src: "/photos/2.jpg",
+            preview: "/photos/preview/2.jpg",
+            description: "Norway 2",
           },
           {
-            id: 1,
-            src: "/photos/1.jpg",
-            preview: "/photos/preview/1.jpg",
-            description: "Norway 1",
+            id: 3,
+            src: "/photos/3.jpg",
+            preview: "/photos/preview/3.jpg",
+            description: "Norway 3",
           },
           {
-            id: 1,
-            src: "/photos/1.jpg",
-            preview: "/photos/preview/1.jpg",
-            description: "Norway 1",
+            id: 4,
+            src: "/photos/4.jpg",
+            preview: "/photos/preview/4.jpg",
+            description: "Norway 4",
           },
           {
-            id: 1,
-            src: "/photos/1.jpg",
-            preview: "/photos/preview/1.jpg",
-            description: "Norway 1",
+            id: 5,
+            src: "/photos/5.jpg",
+            preview: "/photos/preview/5.jpg",
+            description: "Norway 5",
           },
           {
-            id: 1,
-            src: "/photos/1.jpg",
-            preview: "/photos/preview/1.jpg",
-            description: "Norway 1",
+            id: 6,
+            src: "/photos/6.jpg",
+            preview: "/photos/preview/6.jpg",
+            description: "Norway 6",
+          },
+          {
+            id: 7,
+            src: "/photos/7.jpg",
+            preview: "/photos/preview/7.jpg",
+            description: "Norway 7",
           },
         ]}
       />
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,52 @@
-import { FC } from "react";
-interface GalleryProps {}
-const Gallery: FC<GalleryProps> = () => {
-  return <div></div>;
-};
+import { useState } from "react";
 
-export default Gallery;
+import { Photo } from "./types";
+
+import { Navigation } from "./Navigation";
+import { PreviewGallery } from "./PreviewGallery";
+import { TransitionPhoto } from "./MainPhoto/index";
+
+import style from "./index.module.scss";
+
+interface WolfGalleryProps {
+  photos: Photo[];
+}
+
+export const WolfGallery: React.FC<WolfGalleryProps> = ({ photos }) => {
+  if (!photos.length) {
+    return null;
+  }
+
+  const [indexActivePhoto, setIndexActivePhoto] = useState(0);
+  const prevPhoto = photos[indexActivePhoto - 1];
+  const nextPhoto = photos[indexActivePhoto + 1];
+
+  return (
+    <div className={style.wolfGallery}>
+      <div className={style.wolfGalleryContainer}>
+        <TransitionPhoto
+          className={style.wolfGalleryTransitionPhoto}
+          photos={photos}
+          indexActivePhoto={indexActivePhoto}
+        />
+        <Navigation
+          className={style.wolfGalleryNavigation}
+          disabledPrev={!prevPhoto}
+          disabledNext={!nextPhoto}
+          onPrevClick={() => {
+            setIndexActivePhoto(indexActivePhoto - 1);
+          }}
+          onNextClick={() => {
+            setIndexActivePhoto(indexActivePhoto + 1);
+          }}
+        />
+      </div>
+      <PreviewGallery
+        className={style.wolfGalleryPreviewList}
+        indexActivePhoto={indexActivePhoto}
+        photos={photos}
+        setNewPhoto={setIndexActivePhoto}
+      />
+    </div>
+  );
+};
